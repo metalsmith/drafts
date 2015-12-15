ab4ac89b90a84e40a4c516867ebe1cbc3f06f3ac
Stefan Walther <swr@qlik.com>
2015-12-15 17:02:12 +0100

    Fix test to run the current test with a structure containing several fixture-sets.

--- test/index.js
+++ test/index.js
@@ -2,14 +2,22 @@
 var equal = require('assert-dir-equal');
 var Metalsmith = require('metalsmith');
 var drafts = require('..');
+var del = require('del');
 
 describe('metalsmith-drafts', function(){
-  it('should remove drafts from output', function(done){
-    Metalsmith('test/fixture')
+
+  beforeEach( function( done ) {
+        del( 'test/**/build' ).then( function (  ) {
+            done();
+        })
+  });
+
+  it('should remove drafts from output (default behavior)', function(done){
+    Metalsmith('test/fixture/default')
       .use(drafts())
       .build(function(err){
         if (err) return done(err);
-        equal('test/fixture/expected', 'test/fixture/build');
+        equal('test/fixture/default/expected', 'test/fixture/default/build');
         done();
       });
   });
